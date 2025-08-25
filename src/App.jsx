ce36ba66cf17ba6027f09dd5db1e10402d8c415f
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-25 15:25:26 +0200

    A11y for nav. Update of README.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import News from "./components/News";
@@ -11,11 +11,24 @@ function App() {
     setIsOpen(!isOpen);
   }
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
   return (
-    <main
-      className={` flex justify-center  ${!isOpen ? "bg-cust-Off-white" : ""}`}
-    >
-      <div className="flex flex-col gap-4 px-5 py-8 mb-20 font-inter md:mb-0 lg:py-16 lg:px-32  2xl:max-w-7xl">
+    <main className="flex justify-center bg-cust-Off-white relative">
+      {isOpen && (
+        <div className="fixed inset-0 bg-black/60 pointer-events-none" />
+      )}
+      <div className="flex flex-col gap-4 px-5 py-8 mb-6 font-inter md:mb-0 lg:py-16 lg:px-32  2xl:max-w-7xl">
         <Header toggleOpen={toggleOpen} isOpen={isOpen} />
         <div className="md:grid md:grid-cols-2 md:gap-8  lg:grid-cols-[2fr_1fr] ">
           <Hero />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,8 @@ export default function Header({ isOpen, toggleOpen }) {
         className="absolute right-10 z-50 md:hidden"
         onClick={toggleOpen}
         aria-label="Toggle menu"
+        id="menubutton"
+        aria-controls="menu"
       >
         {!isOpen ? (
           <img className="w-12 h-8" src="/icon-menu.svg" alt="Menu hamburger" />
@@ -19,35 +21,42 @@ export default function Header({ isOpen, toggleOpen }) {
         )}
       </button>
       {/* Mobile */}
-      <ul
-        className={`${
-          !isOpen
-            ? "hidden"
-            : "absolute right-0 top-0 flex flex-col gap-6 py-42 px-6 bg-cust-Off-white w-64 h-screen  md:hidden"
-        } `}
-      >
-        {navItems.map((item, index) => (
-          <li
-            className="text-cust-Dark-grayish-blue text-xl hover:text-cust-Soft-red cursor-pointer"
-            key={index}
-          >
-            <a href="#">{item}</a>
-          </li>
-        ))}
-      </ul>
+      <nav>
+        <ul
+          className={`${
+            !isOpen
+              ? "hidden"
+              : "absolute right-0 top-0 flex flex-col gap-6 py-42 px-6 bg-cust-Off-white w-64 h-screen  md:hidden"
+          } `}
+          role="menu"
+          aria-labelledby="menubutton"
+        >
+          {navItems.map((item, index) => (
+            <li
+              className="text-cust-Dark-grayish-blue text-xl hover:text-cust-Soft-red cursor-pointer"
+              key={index}
+            >
+              <a role="menuitem" href="#">
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
       {/* Tablet and Desktop */}
-      <ul className="hidden md:flex md:gap-4 lg:gap-12">
-        {navItems.map((item, index) => (
-          <li
-            className="text-cust-Dark-grayish-blue text-xl hover:text-cust-Soft-red cursor-pointer"
-            key={index}
-          >
-            <a href="#">{item}</a>
-          </li>
-        ))}
-      </ul>
+      <nav>
+        <ul className="hidden md:flex md:gap-4 lg:gap-12" role="menu">
+          {navItems.map((item, index) => (
+            <li
+              className="text-cust-Dark-grayish-blue text-xl hover:text-cust-Soft-red cursor-pointer"
+              key={index}
+            >
+              <a href="#">{item}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
-
-// md:flex md:gap-4 lg:gap-12
