f43d131e0ba9abeb1131e683f4150d9b88d280d5
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-25 14:32:03 +0200

    mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import News from "./components/News";
 import Cards from "./components/Cards";
+
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleOpen() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <main className="bg-cust-Off-white flex justify-center">
-      <div className="flex flex-col gap-4 px-5 py-8 font-inter  lg:py-16 lg:px-32  2xl:max-w-7xl">
-        <Header />
+    <main
+      className={` flex justify-center  ${!isOpen ? "bg-cust-Off-white" : ""}`}
+    >
+      <div className="flex flex-col gap-4 px-5 py-8 mb-20 font-inter md:mb-0 lg:py-16 lg:px-32  2xl:max-w-7xl">
+        <Header toggleOpen={toggleOpen} isOpen={isOpen} />
         <div className="md:grid md:grid-cols-2 md:gap-8  lg:grid-cols-[2fr_1fr] ">
           <Hero />
           <News />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,53 @@
-export default function Header() {
+export default function Header({ isOpen, toggleOpen }) {
+  const navItems = ["Home", "New", "Popular", "Trending", "Categories"];
   return (
     <header className="flex justify-between items-center">
       <img className="w-12 " src="/logo.svg" alt="W. logo" />
-      <button>
-        <img src="/icon-menu.svg" alt="Menu hamburger" />
+      <button
+        className="absolute right-10 z-50 md:hidden"
+        onClick={toggleOpen}
+        aria-label="Toggle menu"
+      >
+        {!isOpen ? (
+          <img className="w-12 h-8" src="/icon-menu.svg" alt="Menu hamburger" />
+        ) : (
+          <img
+            className="w-12 h-8"
+            src="/icon-menu-close.svg"
+            alt="Menu hamburger"
+          />
+        )}
       </button>
+      {/* Mobile */}
+      <ul
+        className={`${
+          !isOpen
+            ? "hidden"
+            : "absolute right-0 top-0 flex flex-col gap-6 py-42 px-6 bg-cust-Off-white w-64 h-screen  md:hidden"
+        } `}
+      >
+        {navItems.map((item, index) => (
+          <li
+            className="text-cust-Dark-grayish-blue text-xl hover:text-cust-Soft-red cursor-pointer"
+            key={index}
+          >
+            <a href="#">{item}</a>
+          </li>
+        ))}
+      </ul>
+      {/* Tablet and Desktop */}
+      <ul className="hidden md:flex md:gap-4 lg:gap-12">
+        {navItems.map((item, index) => (
+          <li
+            className="text-cust-Dark-grayish-blue text-xl hover:text-cust-Soft-red cursor-pointer"
+            key={index}
+          >
+            <a href="#">{item}</a>
+          </li>
+        ))}
+      </ul>
     </header>
   );
 }
+
+// md:flex md:gap-4 lg:gap-12
